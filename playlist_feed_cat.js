6237e9547d96503d43e51f40aabc974dd79d01be
tottokoooo <sselina2000@gmail.com>
2020-06-06 01:49:00 +0800

    youtube api complete

---
+++ playlist_feed_cat.js
@@ -0,0 +1,48 @@
+var player; //YouTube播放器
+
+//當YouTube API準備好時
+function onYouTubeIframeAPIReady(){
+    player = new YT.Player("player",
+        {
+            height:"390",
+            width:"640",
+            videoId:"GAKy6m83yME",
+            playerVars:{
+                "autoplay":1, //自動撥放
+                "controls":0, //控制項
+                "start":20, //起始秒數
+                //"showinfo":0, //上方標題顯示(2018/9已關掉此功能)
+                //"erl":0, //影片最後的推薦影片(2018/9後還是會顯示)
+                "iv_load_policy":3, //不顯示影片註解行銷
+            },
+            events:{
+                "onReady":onPlayerReady,
+                "onStateChange":onPlayerStateChange
+            }
+        }
+    );
+}
+//當YouTube播放器準備好時
+function onPlayerReady(event){
+    player.playVideo();
+}
+
+//當播放器播放狀態改變時
+
+function onPlayerStateChange(event){
+    console.log("change");
+    //目前撥放秒數與預期撥放結束秒數相同時，去撥下一首
+    if(player.getPlayerState()==0){
+        player.cueVideoById({
+            "autoplay":1, //自動撥放
+            "videoId":"GAKy6m83yME",
+            "startSeconds":20,
+            //"suggestedQuality":"large"
+        });
+        console.log("end song");
+    }
+}
+
+$(document).ready(function(){
+    $("#player").hide();
+});
---
+++ playlist_mine_test.js
@@ -0,0 +1,31 @@
+var player; //YouTube播放器
+
+//當YouTube API準備好時
+function onYouTubeIframeAPIReady(){
+    player = new YT.Player("player",
+        {
+            height:"390",
+            width:"640",
+            videoId:"BeI6an1Fy6E",
+            playerVars:{
+                "autoplay":1, //自動撥放
+                "controls":0, //控制項
+                "start":7, //起始秒數
+                //"showinfo":0, //上方標題顯示(2018/9已關掉此功能)
+                //"erl":0, //影片最後的推薦影片(2018/9後還是會顯示)
+                "iv_load_policy":3, //不顯示影片註解行銷
+            },
+            events:{
+                "onReady":onPlayerReady
+            }
+        }
+    );
+}
+//當YouTube播放器準備好時
+function onPlayerReady(event){
+    player.playVideo();
+}
+
+$(document).ready(function(){
+    $("#player").hide();
+});
