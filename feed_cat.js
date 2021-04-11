e3269bad63e9d3cce6e18997dbd4e1eb52ac7762
tottokoooo <sselina2000@gmail.com>
2021-04-11 11:09:53 +0800

    record name and type in localstorage

--- feed_cat.js
+++ feed_cat.js
@@ -1,92 +1,101 @@
 let today = new Date();
-let birthday = new Date;
-function setBirthday(){
-    birthday.setMonth(birthday.getMonth());
-    birthday.setDate(birthday.getDate()-1);
-    birthday.setHours(birthday.getHours());
-    birthday.setMinutes(birthday.getMinutes());
+let birthday = new Date();
+function setBirthday() {
+  birthday.setMonth(birthday.getMonth());
+  birthday.setDate(birthday.getDate() - 1);
+  birthday.setHours(birthday.getHours());
+  birthday.setMinutes(birthday.getMinutes());
 }
 
-function changeColor(){$("#start_button").css("color", "black");}
-function changeColorBack(){$("#start_button").css("color", "gray");}
+function changeColor() {
+  $("#start_button").css("color", "black");
+}
+function changeColorBack() {
+  $("#start_button").css("color", "gray");
+}
 
 let life = 100;
-var myVar=setInterval(function(){update_life()},10000);
-function update_life(){
-    life-=25;
-    console.log(life);
-    //更新生命值圖片
-    $("#life").attr("src","images/life"+life+".png");
-    if(life==-25){
-        clearTimeout(myVar);
-        document.location.href="game_over.html";
-    }
+var myVar = setInterval(function () {
+  update_life();
+}, 1000000);
+function update_life() {
+  life -= 25;
+  console.log(life);
+  //更新生命值圖片
+  $("#life").attr("src", "images/life" + life + ".png");
+  if (life == -25) {
+    clearTimeout(myVar);
+    document.location.href = "game_over.html";
+  }
 }
 
 let beginx_m = [10, 320, 630];
 
 let SetMinute = 0;
 function Check_Time() {
-    SetMinute += 1;
-    let Check_i = document.getElementById("Check_i");
+  SetMinute += 1;
+  let Check_i = document.getElementById("Check_i");
 
-    let Cal_Hour = Math.floor(SetMinute / 3600);
-    let Cal_Minute = Math.floor(Math.floor(SetMinute % 3600) / 60);
+  let Cal_Hour = Math.floor(SetMinute / 3600);
+  let Cal_Minute = Math.floor(Math.floor(SetMinute % 3600) / 60);
 
-    Check_i.innerHTML = Cal_Hour + ":" + Cal_Minute;
+  Check_i.innerHTML = Cal_Hour + ":" + Cal_Minute;
 }
 let mm = window.setInterval("Check_Time()", 1000);
 
-$(document).ready(function(){
-    ctx = $("#home")[0].getContext("2d");
+$(document).ready(function () {
+  ctx = $("#home")[0].getContext("2d"); //畫筆
 
-    imgCat = new Image();
-    food = new Image();
-    let cat_name;
-    let githubURL = new URL(location.href);
-    //alert('location.search: '+location.search);
-    let params = githubURL.searchParams;
-    for (let pair of params.entries()) {
-        cat_name = `${pair[1]}`;
+  imgCat = new Image();
+  food = new Image();
+  let cat_name = localStorage.getItem("type");
+  // let githubURL = new URL(location.href);
+  // //alert('location.search: '+location.search);
+  // let params = githubURL.searchParams;
+  // for (let pair of params.entries()) {
+  //   cat_name = `${pair[1]}`;
+  // }
+
+  imgCat.src = "images/" + cat_name + ".jpg";
+
+  imgCat.onload = function () {
+    let beginx = 0; //裁減圖片的x軸座標，由左往右遞增
+    setInterval(() => {
+      ctx.clearRect(60, 50, 150, 150);
+      console.log("draw cat");
+      ctx.drawImage(imgCat, beginx_m[beginx], 10, 300, 300, 60, 50, 150, 150);
+      beginx++;
+      beginx %= 3;
+    }, 500);
+  };
+
+  $("#birthday").text(birthday.toLocaleDateString().slice(5));
+
+  $("#only_button").click(function () {
+    if (life >= 100) {
+      console.log("我飽了!");
+    } else {
+      life += 25;
+      console.log("feed");
+      console.log(life);
+      //食物圖
+      let randomChildNumber = Math.floor(Math.random() * 3);
+      if (randomChildNumber == 0) {
+        food.src = "images/fish.png";
+      } else if (randomChildNumber == 1) {
+        food.src = "images/can.png";
+      } else {
+        food.src = "images/milk.png";
+      }
+      console.log("draw food:", food);
+      food.onload = function () {
+        ctx.drawImage(food, 0, 0, 300, 300, 220, 148, 100, 100);
+      };
+
+      setTimeout(function () {
+        $("#life").attr("src", "images/life" + life + ".png");
+        ctx.clearRect(220, 148, 100, 100);
+      }, 3000);
     }
-
-	imgCat.src = "images/"+cat_name+".jpg";
-
-    imgCat.onload = function(){
-        let beginx=0; //裁減圖片的x軸座標，由左往右遞增
-        setInterval(() => {
-            ctx.clearRect(60,50,150,150);
-            console.log("draw cat");
-            ctx.drawImage(imgCat, beginx_m[beginx], 10, 300, 300, 60, 50, 150, 150);
-            beginx++;
-            beginx%=3;
-        }, 500);
-    }
-
-    $("#birthday").text(birthday.toLocaleDateString().slice(5));
-
-    $("#only_button").click(function(){
-        if(life>=100){
-            console.log("我飽了!");
-        }
-        else{
-            life+=25;
-            console.log("feed");
-            console.log(life);
-            //食物圖
-            let randomChildNumber = Math.floor(Math.random()*3);
-            if(randomChildNumber==0){food.src = "images/fish.png";}
-            else if(randomChildNumber==1){food.src = "images/can.png";}
-            else{food.src = "images/milk.png";}
-            console.log("draw food:",food);
-            food.onload = function(){ctx.drawImage(food, 0, 0, 300, 300, 220, 148, 100, 100);}
-
-            setTimeout(function(){
-                $("#life").attr("src","images/life"+life+".png");
-                ctx.clearRect(220,148,100,100);
-            },3000);
-        }
-
-    });
+  });
 });
-
--- mine_main.js
+++ mine_main.js
@@ -1,60 +1,88 @@
-$(document).ready(function(){
-    $("#only_button").hide();
-    let currentQuiz=null;
-    $("#startButton").click(function(){
-        if(currentQuiz==null){
-            currentQuiz=0;
-            //顯示題目
-            $("#question").text(questions[0].question);
+$(document).ready(function () {
+  $("#only_button").hide();
+  let currentQuiz = null;
+  $("#startButton").click(function () {
+    if (currentQuiz == null) {
+      currentQuiz = 0;
+      //顯示題目
+      $("#question").text(questions[0].question); //放上第一題
+      $("#options").empty();
+      for (let x = 0; x < questions[0].answers.length; x++) {
+        $("#options").append(
+          "<input name='options' type='radio' value=" +
+            x +
+            "<label>" +
+            questions[0].answers[x][0] +
+            "</label><br><br>"
+        );
+      }
+      //將按鈕文字換成next
+      $("#startButton").attr("value", "Next"); //attr: 更動它的屬性
+    } else {
+      //尋訪每個選項是否有被選取
+      $.each($(":radio"), function (i, val) {
+        if (val.checked) {
+          if (isNaN(questions[currentQuiz].answers[i][1])) {
+            let finalResult = questions[currentQuiz].answers[i][1];
+            $("#question").text("幫她取個名字吧:");
+            $("#font").hide();
             $("#options").empty();
-            for(let x=0; x<questions[0].answers.length;x++){
-                $("#options").append(
-                    "<input name='options' type='radio' value="+
-                    x+
-                    "<label>"+questions[0].answers[x][0]+
-                    "</label><br><br>"
-                );
-            }
-            //將按鈕文字換成next
-            $("#startButton").attr("value","Next"); //attr: 更動它的屬性
-        }else{
-            //尋訪每個選項是否有被選取
-            $.each(
-                $(":radio"),function(i,val){
-                    if(val.checked){
-                        if(isNaN(questions[currentQuiz].answers[i][1])){
-                            let finalResult = questions[currentQuiz].answers[i][1];
-                            $("#question").text(finalAnswers[finalResult][0]);
-                            $("#font").hide();
-                            $("#options").empty();
-                            $("#options").append('<img src=images/'+finalAnswers[finalResult][1]+'_single.jpg><br><br>');
-                            $("#question").append("<input type='text' id='cat_name' name='cat_name' value='"+finalAnswers[finalResult][1]+"'>");
-                            $("#cat_name").hide();
-                            currentQuiz=null;
-                            $("#startButton").hide();
-                            //放出餵養牠的按鈕!!!
-                            $("#only_button").show();
-                            if($("#question_cat_img")){$("#question_cat_img").hide();}
-                        }else{
-                            currentQuiz = questions[currentQuiz].answers[i][1]-1;
-                            $("#question").text(questions[currentQuiz].question);
-                            $("#options").empty();
-                            for(let x=0;x<questions[currentQuiz].answers.length;x++){
-                                $("#options").append(
-                                    "<input name='options' type='radio' value"+
-                                    x+
-                                    "<label>"+questions[currentQuiz].answers[x][0]+
-                                    "</label><br><br>"
-                                );
-                            }
-                        }
-                        return false;
-                    }
-                }
+            $("#options").append(
+              "<img src=images/" +
+                finalAnswers[finalResult][1] +
+                "_single.jpg><br><br>"
             );
+            //放取名字框框
+            // $("#question").append(
+            //   "<input type='text' id='cat_name' class='textContent' name='cat_name' value='" +
+            //     finalAnswers[finalResult][1] +
+            //     "'>"
+            // );
+            $("#question").append(
+              "<input type='text' id='cat_name' class='textContent' name='cat_name'>"
+            );
+
+            currentQuiz = null;
+            $("#startButton").hide();
+            //放出餵養牠的按鈕!!!
+            $("#only_button").show();
+            if ($("#question_cat_img")) {
+              $("#question_cat_img").hide();
+            }
+
+            //let st1 = document.querySelector(".textContent").value;
+            // localStorage.setItem(
+            //   "naga",
+            //   document.querySelector(".textContent").value
+            // ); //存名字
+            localStorage.setItem("type", finalAnswers[finalResult][1]); //存種類
+          } else {
+            currentQuiz = questions[currentQuiz].answers[i][1] - 1;
+            $("#question").text(questions[currentQuiz].question);
+            $("#options").empty();
+            for (let x = 0; x < questions[currentQuiz].answers.length; x++) {
+              $("#options").append(
+                "<input name='options' type='radio' value" +
+                  x +
+                  "<label>" +
+                  questions[currentQuiz].answers[x][0] +
+                  "</label><br><br>"
+              );
+            }
+          }
+          return false;
         }
-    });
+      });
+    }
+  });
+  $("#only_button").click(function () {
+    localStorage.setItem("naga", document.querySelector(".textContent").value); //存名字
+  });
 });
 
-function changeColor(){$("#start_button").css("color", "black");}
-function changeColorBack(){$("#start_button").css("color", "gray");}
+function changeColor() {
+  $("#start_button").css("color", "black");
+}
+function changeColorBack() {
+  $("#start_button").css("color", "gray");
+}
