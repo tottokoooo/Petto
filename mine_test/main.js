dc0d8dce4aadf795af3aedb4a0b15816bb32b3c6
tottokoooo <sselina2000@gmail.com>
2021-04-13 21:23:45 +0800

    mine_test

--- mine_test/main.js
+++ mine_test/main.js
@@ -1,72 +1,68 @@
 $(document).ready(() => {
   $("#only_button").hide();
-  let currentQuiz = null; //建立currentQuiz，儲存目前做達到第幾題
+  let currentQuiz = null;
+
+  //當按鈕被按下時
   $("#startButton").click(() => {
-    //還沒開始作答，從此開始
+    //還未開始答題
     if (currentQuiz == null) {
+      //放上題目跟選項
       currentQuiz = 0;
-      //顯示題目
-      $("#question").text(questions[0].question); //放上第一題(questions[0])的題目(.question)
-      $("#options").empty(); //清空選項區域
-      //加入選項
+      $("#question").text(questions[0].question);
+      $("options").empty();
       for (let x = 0; x < questions[0].answers.length; x++) {
         $("#options").append(
-          "<label><input name='options' type='radio' value=" +
+          "<lable><input name='options' type='radio' value=" +
             x +
             ">" +
             questions[0].answers[x][0] +
-            "</label><br><br>"
+            "</lable><br><br>"
         );
       }
-      //將按鈕文字換成next
-      $("#startButton").attr("value", "Next"); //attr: 更動它的屬性
+      $("startButton").attr("value", "next");
     } else {
-      //若已經開始作答，則從此開始
-      //尋訪每個選項是否有被選取
-      $.each($(":radio"), (i, val) => {
-        console.log(i);
-        console.log(val);
-        //使否產了最後結果
+      $.each($(":radio"), function (i, val) {
         if (val.checked) {
-          //.checked 用來返回checkbox是否被選中
-          if (isNaN(questions[currentQuiz].answers[i][1])) {
-            let finalResult = questions[currentQuiz].answers[i][1];
-            $("#question").text("幫她取個名字吧:");
-            $("#options").empty();
-            $("#question_cat_img img").attr(
-              "src",
-              "../images/" + finalAnswers[finalResult][1] + "_single.jpg"
-            ); //放上對應的貓咪圖
-            $("#question").append(
-              "<input type='text' id='cat_name' class='textContent' name='cat_name' value='" +
-                finalAnswers[finalResult][1] +
-                "'>"
-            ); //放取名字框框
-            currentQuiz = null; //將目前答題數清空
-            $("#startButton").hide();
-            $("#only_button").show(); //放出餵養牠的按鈕
-
-            localStorage.setItem("type", finalAnswers[finalResult][1]); //紀錄種類
-            localStorage.setItem("life", "100"); //初始生命值:滿血
-            localStorage.setItem("age_second", "0"); //初始年紀:0秒
-            let birthday = new Date(); //取今天的日期
-            localStorage.setItem("birth", birthday); //存他的生日
-          } else {
-            //換下一題
+          finalAnswers[questions[currentQuiz].answers[i][1]].score++;
+          if (currentQuiz != questions.length - 1) {
             currentQuiz++;
             $("#question").text(questions[currentQuiz].question);
             $("#options").empty();
             for (let x = 0; x < questions[currentQuiz].answers.length; x++) {
               $("#options").append(
-                "<label><input name='options' type='radio' value" +
+                "<lable><input name='options' type='radio' value=" +
                   x +
                   ">" +
                   questions[currentQuiz].answers[x][0] +
-                  "</label><br><br>"
+                  "</lable><br><br>"
               );
             }
+          } else {
+            currentQuiz = null;
+            $("#question").text("幫她取個名字吧:");
+            $("#options").empty();
+            $("#startButton").hide();
+            $("#only_button").show();
+
+            // sort by value
+            finalAnswers.sort(function (a, b) {
+              return b.score - a.score;
+            });
+            $("#question_cat_img img").attr(
+              "src",
+              "../images/" + finalAnswers[0].en + "_single.jpg"
+            ); //放上對應的貓咪圖
+            $("#question").append(
+              "<input type='text' id='cat_name' class='textContent' name='cat_name' value='cat_name'>"
+            ); //放取名字框框
+
+            localStorage.setItem("type", finalAnswers[0].en); //紀錄種類
+            localStorage.setItem("life", "100"); //初始生命值:滿血
+            localStorage.setItem("age_second", "0"); //初始年紀:0秒
+            let birthday = new Date(); //取今天的日期
+            localStorage.setItem("birth", birthday); //存他的生日
           }
-          return false; //完成後即可跳離迴圈
+          return false;
         }
       });
     }
